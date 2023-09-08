61773565e562e374081aaf4ab35967273e7b010c
arpit-oss <137763018+arpit-oss@users.noreply.github.com>
2023-09-08 17:50:45 +0530

    Add files via upload

---
+++ index.html.php
@@ -0,0 +1,266 @@
+<?php include("header.php");?>
+
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+   
+  </div>
+
+
+  <div class="carousel-inner ">
+    <div class="carousel-item active">
+      <img src="images/car.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+         <div class="p-3 " style="margin-bottom: 300px;">
+           <h3 >Luxury Living</h3>
+            <h1 style="font-size: 40px ; color: black;font-weight:bold;">Discover A Brand Luxurious Hotel</h1>
+            <a href="#" class="btn btn-primary" >Our Rooms</a>
+            <a href="#" class="btn btn-light ">Book A Room</a>
+          </div>
+          </div>
+        </div>
+
+    <div class="carousel-item">
+           <img src="images/car1.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+          <div class="p-3" style="max-width: 700px;">
+          <h3 >Luxury Living</h3>
+           <h1 style="font-size: bold; color: black;font-weight: 600;" >Discover A Brand Luxurious Hotel</h1>
+           <a href="#" class="btn btn-primary ">Our Rooms</a>
+            <a href="#" class="btn btn-light ">Book Rooms</a>
+          </div>
+      </div>
+    </div>
+    
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+<!--
+<section class="our">
+<div class="mt-3">
+<h1 class="text-center" tyle="font-weight:bold;">Our
+<span class="text-danger"> Room</span></h1>
+</div>
+
+   
+   <div class="d-flex justify-content-between border-bottom ">
+
+    <div class="shadow p-3 mb-2 rounded text-center" style="background-color: #fde3a7;" > 
+
+  <div class="row mt-3 m-auto mx-auto ">
+
+<div class="col-md-4 col-sm-12 col-lg-4 ">
+    <div class="card " style="width: 18rem;">
+     <img src="img/room-1.jpg" class="card-img-top" alt="...">
+     <div class="card-body">
+      <p class="card-text"style="text-align:justify;">That’s pretty important. Planning is vital if you’re to make the most of your time away. You really don’t want to get there with a cluttered mind and walk around not knowing what to do next!</p>
+     <a href="#" class="btn btn-primary">Book Now</a>
+    </div>
+  </div>
+</div>
+
+<div class="col-md-4 col-sm-12 col-lg-4">
+<div class="card" style="width: 18rem;">
+  <img src="img/room-2.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text"style="text-align:justify;">That’s pretty important. Planning is vital if you’re to make the most of your time away. You really don’t want to get there with a cluttered mind and walk around not knowing what to do next! </p>
+    <a href="#" class="btn btn-primary" role='button'>Book Now</a>
+  </div>
+</div>
+</div>
+
+<div class="col-md-4 col-sm-12 col-lg-4 ">
+<div class="card" style="width: 18rem;">
+  <img src="img/room-3.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text" style="text-align:justify;">That’s pretty important. Planning is vital if you’re to make the most of your time away. You really don’t want to get there with a cluttered mind and walk around not knowing what to do next!</p>
+    <a href="#" class="btn d-inline p-2 bg-primary text-white ">Book Now</a>
+    
+  </div>
+</div>
+</div>
+      <a href="#" class="btn btn-danger mt-5 mx-auto" style="width:160px;">View All</a>
+</div>
+</div>
+</div>
+</div>
+
+</section>
+-->
+
+
+<section class="About">
+<h1 class="text-center" style="font-weight:bold;">About
+          <span class="text-danger"> Us</span></h1>
+  <div class="container">
+    <div class="row g-0">
+
+      <div class="col-md-6 bg-dark d-flex align-items-center">
+        <div class="py-3">
+         <h1 class="mr-2 text-white text-center "> Discover A Brand Luxurious<span class="text-danger">Hotel</span class="text-danger"><span class="text-danger"> !</span> </h1>
+         <p class="text-white text-center " style="text-align: justify;">Luxury hotel marketing is a specific type of marketing.it’s important to utilize retail principles and leverage your brand when designing your space.
+         </p>
+        </div>
+      </div>
+
+      <div class="col-md-6 bg-dark d-flex align-items-center">
+       <img src="img/about-1.jpg" class="w-100">
+      </div>
+
+    </div>
+  </div>
+
+        <div class="container-fluid">
+       <div class="row row-cols-1 row-cols-md-4 mt-5">
+        <div class="col-lg-6 col-sm-12 ">
+          <h1 class="text-center" style="font-weight:bold;">Welcome to
+          <span class="text-danger"> HOTEL</span></h1>
+          <p style="text-align:justify;">Whether you’re running an independent optical shop or you’ve opened an optical dispensary for your optometric practice, it’s important to utilize retail principles and leverage your brand when designing your space. Consideration of details such as your optical showroom layout can make an outsize difference in sales performance, in addition to how visitors think of your brand.Whether you’re running an independent optical shop or you’ve opened an optical dispensary for your optometric practice, it’s important to utilize retail principles and leverage your brand when designing your space. Consideration of details such as your optical showroom layout can make an outsize difference in sales performance, in addition to how visitors think of your brand.Whether you’re running an independent optical shop or you’ve opened an optical dispensary for your optometric practice, it’s important to utilize retail principles and leverage your brand when designing your space. Consideration of details such as your optical showroom layout can make an outsize difference in sales performance, in addition to how visitors think of your brand.</p>
+         </div>
+
+           <div class="col-lg-6 col-sm-12 g-3 py-2">
+            <img src="img/carousel-1.jpg" class="w-100" style="height:350px;">
+           </div>
+        
+        </div>
+       </div>
+</section>
+ 
+ <div class="shadow p-3 mb-5 bg-body rounded mt-2">
+ <div class="text-center ">
+      <h1 style="font-weight: bold;">Testi<span class="text-danger">monial</span></h1>
+</div>
+
+<div class="d-flex justify-content-around mt-4">
+  
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="card bg-dark">
+      <img src="img/testimonial-1.jpg" class="rounded-circle mx-auto" style="width: 30%;height: 20%;">
+      <div class="card-body">
+        <h5 class="card-title text-center text-primary">Head</h5>
+        <p class="card-text text-white">"This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer".</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card bg-dark">
+      <img src="img/testimonial-2.jpg" class="rounded-circle mx-auto" style="width: 30%;height: 20%;">
+      <div class="card-body">
+        <h5 class="card-title text-center text-primary">Service Provider</h5>
+        <p class="card-text text-white">"This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer".</p>
+      </div>
+    </div>
+  </div>
+  <div class="col ">
+    <div class="card bg-dark">
+      <img src="img/testimonial-3.jpg" class="rounded-circle mx-auto" style="width: 30%;height: 20%;">
+      <div class="card-body">
+        <h5 class="card-title text-center text-primary">Accountant</h5>
+        <p class="card-text text-white">"This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer".</p>
+      </div>
+    </div>
+  </div>
+  
+  </div>
+
+</div>
+</div>
+
+<div class="owl-carousel owl-theme g-0" >
+    <div class="item"> <img src="img/c8.jpeg" style="width:50%;height: 40%;"></div>
+    <div class="item"> <img src="img/c2.jpeg" style="width: 50%; height:40%;"></div>
+    <div class="item"> <img src="img/c3.jpeg" style="margin-right: 60px;" ></div>
+    <div class="item"> <img src="img/c10.jpeg" style="width: 50%; height:40%;"></div>
+    
+</div>
+<section class="contact">
+  <div class="d-flex justify-content-around bg-dark">
+<div class="row mt-3">
+<h2 class="text-white text-center p-1 border-bottom">Contact<span class="text-danger "> Us</span>
+</h2>
+  <div class="col-md-6 mt-4 ">
+
+    <div class="row ">
+
+  <div class="col g-3 ">
+    <input type="text" class="form-control" placeholder="First name" aria-label="First name">
+  </div>
+
+  <div class="col g-3">
+    <input type="text" class="form-control" placeholder="Last name" aria-label="Last name">
+  </div>
+
+</div>
+<div class="row">
+  <div class="col g-3 mt-5">
+    <input type="number" class="form-control" placeholder="Phone" aria-label="First name">
+  </div>
+  <div class="col g-3 mt-5">
+    <input type="Email" class="form-control" placeholder="Email" aria-label="Last name">
+  </div>
+</div>
+  <div class="col mt-5 ">
+
+  <input type="Message" class="form-control p-5 mt-4" placeholder="Message">
+  </div>
+   <button class="btn btn-danger mt-5 text-white "> Send Message</button>
+  </div>
+
+
+   
+  <div class="col-md-6 mt-4">
+    <iframe width="120%" height="500" frameborder="0" src="https://www.bing.com/maps/embed?h=400&w=500&cp=26.49396613720154~80.27924537658691&lvl=16&typ=d&sty=r&src=SHELL&FORM=MBEDV8" scrolling="no">
+     </iframe>
+   </div>
+  
+
+</div>
+</div>
+
+
+
+
+</section>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" ></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+ 
+ 
+ <script>
+  
+   $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:1000,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+ </script>
+ 
+</body>
+</html>
+<?php include("footer.php");?>
